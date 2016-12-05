a23eb28b3c5518ee2a07ebbe9b4c84287e18a02e
GundamExia <geobudo@gmail.com>
2016-12-05 14:16:37 +0100

    Preparado para recibir varios juegos

--- app/src/main/java/com/example/dam212/flipi/Flipi.java
+++ app/src/main/java/com/example/dam212/flipi/Flipi.java
@@ -2,29 +2,54 @@ package com.example.dam212.flipi;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.TextView;
 
 public class Flipi extends Activity {
-
-    private TextView textViewText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flipi);
 
-        // Bundle que contiene los extras que se hayan puesto desde otra actividad
+        // Bundle que contiene todos los extras.
         Bundle configs = getIntent().getExtras();
 
-        initVariables();
-        receiveBundle(configs);
+        receiveTypeOfGame(configs);
     }
 
-    private void initVariables() {
-        textViewText = (TextView) findViewById(R.id.textViewText);
+    /**
+     * Recibe un Bundle con las variables de las opciones y después se los asigna a las variables
+     * de esta clase.
+     *
+     * @param configs Bundle que contiene las opciones escogidas en el menú principal.
+     */
+    private void receiveTypeOfGame(Bundle configs) {
+        int typeOfGame = configs.getInt("selectedGame");
+
+        // Según el tipo de juego que se haya escogido se recogen unas variables u otras.
+        switch (typeOfGame) {
+            case 0:
+                initFlip(configs.getInt("seekBarRows"), configs.getInt("seekBarColumns"), configs.getInt("seekBarMaxLoop"),
+                        configs.getBoolean("checkBoxSound"), configs.getBoolean("checkBoxHaptic"), "color");
+                break;
+            case 1:
+                initFlip(configs.getInt("seekBarRows"), configs.getInt("seekBarColumns"), configs.getInt("seekBarMaxLoop"),
+                        configs.getBoolean("checkBoxSound"), configs.getBoolean("checkBoxHaptic"), "number");
+                break;
+            default:
+                break;
+        }
     }
 
-    private void receiveBundle(Bundle configs) {
-        textViewText.setText(String.valueOf(configs.getInt("seekBarRows")));
+    /**
+     * Inicia el juego Flip
+     * @param rows int, número de filas
+     * @param columns int, número de columnas
+     * @param maxLoops int, número máximo de bucles
+     * @param sound boolean, para tener o no sonido
+     * @param haptic boolean, para que vibre o no
+     * @param typeOfFlip String, cómo se representarán las casillas
+     */
+    private void initFlip(int rows, int columns, int maxLoops, boolean sound, boolean haptic, String typeOfFlip) {
+
     }
 }
--- app/src/main/java/com/example/dam212/flipi/MainActivity.java
+++ app/src/main/java/com/example/dam212/flipi/MainActivity.java
@@ -5,17 +5,18 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.CheckBox;
-import android.widget.RadioGroup;
 import android.widget.SeekBar;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
     private SeekBar seekBarRows;
     private SeekBar seekBarColumns;
     private SeekBar seekBarMaxLoop;
-    private RadioGroup radioGroup;
     private CheckBox checkBoxSound;
     private CheckBox checkBoxHaptic;
+    private Spinner spinnerTypeOfGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,25 +26,37 @@ public class MainActivity extends AppCompatActivity {
         initVariables();
     }
 
-    // Inicia todas las variables necesarias
+    /**
+     * Inicia los view con sus variables necesarias.
+     */
     private void initVariables() {
         seekBarRows = (SeekBar) findViewById(R.id.seekBarRows);
         seekBarColumns = (SeekBar) findViewById(R.id.seekBarColumns);
         seekBarMaxLoop = (SeekBar) findViewById(R.id.seekBarMaxLoop);
 
-        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
-
         checkBoxSound = (CheckBox) findViewById(R.id.checkBoxSound);
         checkBoxHaptic = (CheckBox) findViewById(R.id.checkBoxHaptic);
+
+        spinnerTypeOfGame = (Spinner) findViewById(R.id.spinnerTypeOfGame);
     }
 
-    // Crea un nuevo intent para iniciar la clase flipi con las variables necesarias
+    /**
+     * Crea un nuevo intent para iniciar la clase flipi con las variables necesarias.
+     * @param v es el View.
+     */
     public void sendBundle(View v) {
-        Intent intentFlipi = new Intent(this, Flipi.class);
+        if(seekBarRows.getProgress() != 0 && seekBarColumns.getProgress() != 0 && seekBarMaxLoop.getProgress() != 0) {
+            Intent intentFlipi = new Intent(this, Flipi.class);
 
-        intentFlipi.putExtra("seekBarRows", seekBarRows.getProgress());
+            intentFlipi.putExtra("seekBarRows", seekBarRows.getProgress());
+            intentFlipi.putExtra("seekBarColumns", seekBarColumns.getProgress());
+            intentFlipi.putExtra("seekBarMaxLoop", seekBarMaxLoop.getProgress());
+            intentFlipi.putExtra("selectedGame", spinnerTypeOfGame.getSelectedItemPosition());
+            if(checkBoxSound.isSelected()) intentFlipi.putExtra("checkBoxSound", true);
+            if(checkBoxHaptic.isSelected()) intentFlipi.putExtra("checkBoxHaptic", true);
 
-        startActivity(intentFlipi);
+            startActivity(intentFlipi);
+        } else Toast.makeText(this, R.string.norowsnocolumnsnomaxloops, Toast.LENGTH_SHORT).show();
     }
 
 }
