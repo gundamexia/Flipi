836b53342ef5a158676a314530f521a169f773a8
gundamexia <geobudo@gmail.com>
2016-12-05 10:40:40 +0100

    Creación de bundles

--- app/src/main/java/com/example/dam212/flipi/Flipi.java
+++ app/src/main/java/com/example/dam212/flipi/Flipi.java
@@ -1,7 +1,26 @@
 package com.example.dam212.flipi;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
 
 public class Flipi extends Activity {
 
+    TextView textViewText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        initVariables();
+        receiveBundle(savedInstanceState);
+    }
+
+    private void initVariables() {
+        textViewText = (TextView) findViewById(R.id.textViewText);
+    }
+
+    private void receiveBundle(Bundle bundleConfig) {
+        textViewText.setText(bundleConfig.getInt("seekBarRows"));
+    }
 }
--- app/src/main/java/com/example/dam212/flipi/MainActivity.java
+++ app/src/main/java/com/example/dam212/flipi/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.dam212.flipi;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.CheckBox;
+import android.widget.RadioGroup;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
+
+    SeekBar seekBarRows;
+    SeekBar seekBarColumns;
+    SeekBar seekBarMaxLoop;
+    RadioGroup radioGroup;
+    CheckBox checkBoxSound;
+    CheckBox checkBoxHaptic;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        initVariables();
+    }
+
+    private void initVariables() {
+        seekBarRows = (SeekBar) findViewById(R.id.seekBarRows);
+        seekBarColumns = (SeekBar) findViewById(R.id.seekBarColumns);
+        seekBarMaxLoop = (SeekBar) findViewById(R.id.seekBarMaxLoop);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radioGroup);
+
+        checkBoxSound = (CheckBox) findViewById(R.id.checkBoxSound);
+        checkBoxHaptic = (CheckBox) findViewById(R.id.checkBoxHaptic);
+    }
+
+    public void sendBundle(View v) {
+        Intent intentFlipi = new Intent(this, Flipi.class);
+
+        intentFlipi.putExtra("seekBarRows", seekBarRows.getProgress());
+
+        startActivity(intentFlipi);
     }
 }
