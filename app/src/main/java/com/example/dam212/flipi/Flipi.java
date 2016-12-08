b0a97b915f74185fa8cafd4a2c2a8890698e4b07
Arceso <arcesone@gmail.com>
2016-12-09 00:29:03 +0100

    Added the Flip game, with it's resources.

--- app/src/main/java/com/example/dam212/flipi/Flipi.java
+++ app/src/main/java/com/example/dam212/flipi/Flipi.java
@@ -2,54 +2,261 @@ package com.example.dam212.flipi;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.app.Service;
+import android.content.Intent;
+import android.media.MediaPlayer;
+import android.os.Vibrator;
+import android.util.DisplayMetrics;
+import android.util.Log;
+import android.view.View;
+import android.widget.Chronometer;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import java.util.Random;
 public class Flipi extends Activity {
 
+    //tipos de juego
+    private final int COLOR = 0;
+    private final int NUMBER = 1;
+
+    //colores
+    private static final int []colors = new int[]{
+            R.drawable.ic_1c,
+            R.drawable.ic_2c,
+            R.drawable.ic_3c,
+            R.drawable.ic_4c,
+            R.drawable.ic_5c,
+            R.drawable.ic_6c
+    };
+
+
+    //numeros
+    private static final int []numbers = new int[]{
+            R.drawable.ic_1n,
+            R.drawable.ic_2n,
+            R.drawable.ic_3n,
+            R.drawable.ic_4n,
+            R.drawable.ic_5n,
+            R.drawable.ic_6n
+    };
+
+
+    //mantener el array que el usuario hay decidido utilizar
+    private int []pictures = null;
+
+    // Número máximo de celdas horizontales y verticales
+    private int topTileX = 3;
+    private int topTileY = 3;
+
+    // Número máximo de elementos a utilizar
+    private int topElements = 2;
+
+    //	Si ha seleccionado o no usar sonido y vibración
+    private boolean hasSound = false;
+    private boolean hasVibration = false;
+
+    // Array con los identificadores de las celdas cuando
+    // se añadan al layout,
+    // para poder recuperarlos durante la partida
+    private int ids[][] = null;
+
+    // Array para guardar los valores de los índices de cada una de las celdas.
+    // se utilizará para agilizar la comprobación de si la partida ha acabado o no.
+    private int values[][] = null;
+
+    // Contador con el número de pulsaciones que ha realizado el jugador
+    private int numberOfClicks = 0;
+
+    // Para reproducir un sonido cuando el usuario pulse una celda
+    private MediaPlayer mp = null;
+
+    // Para hacer vibrar el dispositivo cuando el usuario pulse una celda
+    private Vibrator vibratorService = null;
+
+    // Mostrará en pantalla el las veces que el usuario ha pulsado una celda
+    private TextView tvNumberOfClicks = null;
+
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.flipi);
+        vibratorService = (Vibrator)(getSystemService(Service.VIBRATOR_SERVICE));
+        mp = MediaPlayer.create(this, R.raw.touch);
+        tvNumberOfClicks = (TextView) findViewById(R.id.clicksTxt);
 
-        // Bundle que contiene todos los extras.
-        Bundle configs = getIntent().getExtras();
 
-        receiveTypeOfGame(configs);
-    }
+        //obtención de parámetros de configuración
+        Bundle extras = getIntent().getExtras();
+        topTileX = extras.getInt("Rows") + 2;
+        topTileY = extras.getInt("Columns") + 2;
 
-    /**
-     * Recibe un Bundle con las variables de las opciones y después se los asigna a las variables
-     * de esta clase.
-     *
-     * @param configs Bundle que contiene las opciones escogidas en el menú principal.
-     */
-    private void receiveTypeOfGame(Bundle configs) {
-        int typeOfGame = configs.getInt("selectedGame");
+        topElements = extras.getInt("MaxLoop") + 1;
 
-        // Según el tipo de juego que se haya escogido se recogen unas variables u otras.
-        switch (typeOfGame) {
-            case 0:
-                initFlip(configs.getInt("seekBarRows"), configs.getInt("seekBarColumns"), configs.getInt("seekBarMaxLoop"),
-                        configs.getBoolean("checkBoxSound"), configs.getBoolean("checkBoxHaptic"), "color");
-                break;
-            case 1:
-                initFlip(configs.getInt("seekBarRows"), configs.getInt("seekBarColumns"), configs.getInt("seekBarMaxLoop"),
-                        configs.getBoolean("checkBoxSound"), configs.getBoolean("checkBoxHaptic"), "number");
-                break;
-            default:
-                break;
+        if ( extras.getInt("selectedGame") == COLOR) pictures = colors;
+        else if ( extras.getInt("selectedGame") == NUMBER) pictures = numbers;
+
+        hasSound= extras.getBoolean("Sound");
+        hasVibration= extras.getBoolean("Haptic");
+
+
+        //limpiar el tablero
+        LinearLayout ll = (LinearLayout) findViewById(R.id.fieldLandscape);
+        ll.removeAllViews();
+
+
+        //obtención de tamaño de pantalla
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        int width = dm.widthPixels / topTileX;
+        int height = (dm.heightPixels - 90) / topTileY;
+
+
+        //inicialización de arrays
+        ids =  new int [topTileX][topTileY];
+        values =  new int [topTileX][topTileY];
+
+
+        //inicialización de números aleatorio
+        Random r = new Random(System.currentTimeMillis());
+
+        // crear celdas
+        int ident = 0;
+        for (int i = 0; i < topTileY; i++) {
+            LinearLayout l2 = new LinearLayout(this);
+            l2.setOrientation(LinearLayout.HORIZONTAL);
+            for (int j = 0; j < topTileX; j++) {
+                int tilePictureToShow = r.nextInt(topElements);
+
+                // guardamos la trama a mostrar
+                values[j][i] = tilePictureToShow;
+                TileView tv = new TileView(this, j, i, topElements, tilePictureToShow, pictures[tilePictureToShow]);
+                ident++;
+
+                // se asigna un identificador al objeto creado
+                tv.setId(ident);
+
+                // se guarda el identificador en una matriz
+                ids[j][i] = ident;
+                tv.setHeight(height);
+                tv.setWidth(width);
+                tv.setOnClickListener(new View.OnClickListener() {
+                    public void onClick(View view) {
+                        hasClick(((TileView) view).x, ((TileView) view).y);
+                    }
+                });
+                l2.addView(tv);
+            }
+            ll.addView(l2);
         }
+        // cronómetro
+        Chronometer t = (Chronometer)findViewById(R.id.Chronometer);
+        t.start();
     }
 
-    /**
-     * Inicia el juego Flip
-     * @param rows int, número de filas
-     * @param columns int, número de columnas
-     * @param maxLoops int, número máximo de bucles
-     * @param sound boolean, para tener o no sonido
-     * @param haptic boolean, para que vibre o no
-     * @param typeOfFlip String, cómo se representarán las casillas
-     */
-    private void initFlip(int rows, int columns, int maxLoops, boolean sound, boolean haptic, String typeOfFlip) {
 
+    //Método llamado al clickar en un botón (Definido programáticamente).
+    protected void hasClick(int x, int y) {
+        Log.i("Clicked", "Start of click action");
+        // vibrar y/o sonar si está configurado
+        if (hasVibration) vibratorService.vibrate(100);
+        if (hasSound) mp.start();
+        //cambiar la celda pulsada
+        changeView(x, y);
+        //esquinas del tablero
+        //Superior izquierda
+        if (x==0 && y == 0){
+            changeView(0, 1);
+            changeView(1, 0);
+            changeView(1, 1);
+        }
+        //Inferior izquierda
+        else if (x == 0 && y  == topTileY -1 ){
+            changeView(0, topTileY-2);
+            changeView(1, topTileY-2);
+            changeView(1, topTileY -1);
+        }
+        //Superior derecha
+        else if (x == topTileX -1 && y  == 0 ){ //
+            changeView( topTileX-2,0);
+            changeView( topTileX-2,1);
+            changeView( topTileX -1,1);
+        }
+        //Inferior derecha
+        else if (x == topTileX -1 && y  == topTileY-1 ){
+            changeView( topTileX-2,topTileY-1);
+            changeView( topTileX-2,topTileY-2);
+            changeView( topTileX -1,topTileY-2);
+        }
+        // lados del tablero
+        //Izquierda
+        else if (x == 0){
+            changeView( x,y-1);
+            changeView( x,y+1);
+            changeView( x+1 ,y);
+        }
+        //Arriba
+        else if (y == 0){
+            changeView( x-1,y);
+            changeView( x+1,y);
+            changeView( x ,y+1);
+        }
+        //Derecha
+        else if (x == topTileX-1){
+            changeView( x,y-1);
+            changeView( x,y+1);
+            changeView( x-1 ,y);
+        }
+        //Abajo
+        else if (y == topTileY-1){
+            changeView( x-1,y);
+            changeView( x+1,y);
+            changeView( x ,y-1);
+        }
+        //resto
+        else{
+            changeView( x-1,y);
+            changeView( x+1,y);
+            changeView( x,y-1);
+            changeView( x,y+1);
+        }
+        // actualiza marcador
+        numberOfClicks++;
+        tvNumberOfClicks.setText( getString(R.string.textViewPulsations) + numberOfClicks );
+        // se ha acabado la partida?
+        if (hasFinished()) gameWon();
+    }
+
+    //No estoy muy seguro de como funciona,
+    // pero lo que hace es actualizar las cuadriculas (numero o color) CREO.
+    private void changeView(int x, int y){
+        TileView tt = (TileView) findViewById(ids[x][y]);
+        int newIndex = tt.getNewIndex();
+        values[x][y] = newIndex;
+        tt.setBackgroundResource(pictures[newIndex]);
+        tt.invalidate();
+    }
+
+    //Comprueba que la partida haya acabado.
+    //Es decir que todas las casillas compartan número o color.
+    private Boolean hasFinished() {
+        Log.i("checkIfFinished", "Start of check action");
+        int targetValue = values[0][0];
+        for (int i = 0; i < topTileY; i++) {
+            for (int j = 0; j < topTileX; j++) {
+                if (values[j][i] != targetValue) return false;
+            }
+        }
+        return true;
+    }
+
+
+    //Acaba la partida devolviendo el total de clicks.
+    private void gameWon(){
+        Intent resultIntent = new Intent();
+        Log.i("Number_of_clicks", Integer.toString(numberOfClicks));
+        resultIntent.putExtra("clicks", numberOfClicks);
+        setResult(RESULT_OK, resultIntent);
+        finish();
     }
 }
--- app/src/main/java/com/example/dam212/flipi/MainActivity.java
+++ app/src/main/java/com/example/dam212/flipi/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.dam212.flipi;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.CheckBox;
 import android.widget.SeekBar;
@@ -10,6 +11,8 @@ import android.widget.Spinner;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private final int GAMEFINISHED = 0;
 
     private SeekBar seekBarRows;
     private SeekBar seekBarColumns;
@@ -45,18 +48,29 @@ public class MainActivity extends AppCompatActivity {
      * @param v es el View.
      */
     public void sendBundle(View v) {
-        if(seekBarRows.getProgress() != 0 && seekBarColumns.getProgress() != 0 && seekBarMaxLoop.getProgress() != 0) {
-            Intent intentFlipi = new Intent(this, Flipi.class);
+        Intent intentFlipi = new Intent(this, Flipi.class);
 
-            intentFlipi.putExtra("seekBarRows", seekBarRows.getProgress());
-            intentFlipi.putExtra("seekBarColumns", seekBarColumns.getProgress());
-            intentFlipi.putExtra("seekBarMaxLoop", seekBarMaxLoop.getProgress());
-            intentFlipi.putExtra("selectedGame", spinnerTypeOfGame.getSelectedItemPosition());
-            if(checkBoxSound.isSelected()) intentFlipi.putExtra("checkBoxSound", true);
-            if(checkBoxHaptic.isSelected()) intentFlipi.putExtra("checkBoxHaptic", true);
+        intentFlipi.putExtra("Rows", seekBarRows.getProgress());
 
-            startActivity(intentFlipi);
-        } else Toast.makeText(this, R.string.norowsnocolumnsnomaxloops, Toast.LENGTH_SHORT).show();
+        intentFlipi.putExtra("Columns", seekBarColumns.getProgress());
+        intentFlipi.putExtra("MaxLoop", seekBarMaxLoop.getProgress());
+        intentFlipi.putExtra("selectedGame", spinnerTypeOfGame.getSelectedItemPosition());
+        if(checkBoxSound.isSelected()) intentFlipi.putExtra("Sound", true);
+        if(checkBoxHaptic.isSelected()) intentFlipi.putExtra("Haptic", true);
+
+        startActivityForResult(intentFlipi, GAMEFINISHED);
+    }
+
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        switch(requestCode) {
+            case GAMEFINISHED:
+                if (resultCode == RESULT_OK) {
+                    Bundle result = data.getExtras();
+                    Log.d("RESULT",data.getExtras().getString("clicks"));
+                    Toast.makeText(this, result.getString("clicks"), Toast.LENGTH_LONG).show();
+                }
+                break;
+        }
     }
 
 }
