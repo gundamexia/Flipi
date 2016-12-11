5a2db03d0c6c802da52e47ecbe0d9bb1016977e5
Arceso <arcesone@gmail.com>
2016-12-11 02:37:21 +0100

    Added: A functional but not real scores class (and its associated components). Support for on back press.

---
+++ app/src/main/java/com/example/dam212/flipi/score.java
@@ -0,0 +1,33 @@
+package com.example.dam212.flipi;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ListView;
+
+import java.util.Arrays;
+
+public class Score extends AppCompatActivity{
+    ListAdapter mAdapter;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.score);
+
+        mAdapter = new ListAdapter(this, R.id.listViewScore , Arrays.asList(genTest()) );
+
+        ((ListView)findViewById(R.id.listViewScore)).setAdapter(mAdapter);
+    }
+
+
+    private ListItem[] genTest(){
+        return new ListItem[]{
+                new ListItem("a", "b", "c", "d"),
+                new ListItem("e", "f", "g", "h"),
+                new ListItem("i", "j", "k", "l")
+        };
+    }
+
+}
